6b5879ae505b8102265fe5ba8e2cf9b78e8912f0
Serge Rey <sjsrey@gmail.com>
2019-04-21 17:59:21 -0700

    Added a file from spyder

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy
+
+x = numpy.arange(10)
+y = x / 1.5
+
+# added this to a project in git
+z = x * y
